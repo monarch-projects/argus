859fecb1235e9b13e4a1d7ecd30c421ad1902700
zhengyate <2925776766@qq.com>
2019-07-18 23:25:42 +0800

    架构模块设计划分

---
+++ argus-server/src/main/java/org/titan/argus/server/ArgusServerApplication.java
@@ -0,0 +1,12 @@
+package org.titan.argus.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArgusServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArgusServerApplication.class, args);
+    }
+}
