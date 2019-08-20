c1b07bec41a76ef28adcc1f6e99099b56915d936
starboyate <2925776766@qq.com>
2019-08-20 17:46:42 +0800

    完善hystrix熔断降级功能

--- argus-plugin/argus-plugin-fallback/argus-plugin-fallback-hystrix/src/main/java/org/titan/argus/plugin/fallback/hystrix/web/reactive/ReactiveRequestContext.java
+++ argus-plugin/argus-plugin-fallback/argus-plugin-fallback-hystrix/src/main/java/org/titan/argus/plugin/fallback/hystrix/web/reactive/ReactiveRequestContext.java
@@ -3,7 +3,7 @@ package org.titan.argus.plugin.fallback.hystrix.web.reactive;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 
 public final class ReactiveRequestContext {
-	private static final InheritableThreadLocal<ServerHttpRequest> context = new InheritableThreadLocal();
+	private static final ThreadLocal<ServerHttpRequest> context = new InheritableThreadLocal<>();
 
 	public static void set(ServerHttpRequest request) {
 		context.set(request);
