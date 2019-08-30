38c5b633d9281927d05634720da3240a54417f02
333157 <jiuming.tjm@alibaba-inc.com>
2019-08-30 23:24:51 +0800

    spring data es repo

--- argus-server/src/main/java/org/titan/argus/server/ArgusServerApplication.java
+++ argus-server/src/main/java/org/titan/argus/server/ArgusServerApplication.java
@@ -4,6 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
@@ -12,6 +13,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @MapperScan("org.titan.argus.storage.mysql")
 @EnableScheduling
 @EnableAsync
+@EnableElasticsearchRepositories(basePackages = {"org.titan.argus.storage.es.repo"})
 public class ArgusServerApplication {
 
     public static void main(String[] args) {
